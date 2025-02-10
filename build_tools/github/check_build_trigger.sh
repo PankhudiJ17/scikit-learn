266b4cbac1c4dca8ab1976d0d7faac09a8dbb884
Pankhudi Jain <Pankhudi.Jain@ibm.com>
2025-02-10 15:48:59 +0530

    added code

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -4,7 +4,10 @@ set -e
 set -x
 
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
+echo "Commit message: $COMMIT_MSG"  # Debugging line
 
+# Check the event name and whether the build should be triggered
+echo "Event Name: $GITHUB_EVENT_NAME"  # Debugging line
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == schedule ||
       "$GITHUB_EVENT_NAME" == workflow_dispatch ||
