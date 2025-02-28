42d2fd88011de771cd340fb1e7675339f5eac6df
Pankhudi Jain <Pankhudi.Jain@ibm.com>
2025-02-28 16:25:53 +0530

    Added self hosted runner

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -4,15 +4,15 @@ set -e
 set -x
 
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
-echo "Commit message: $COMMIT_MSG"  # Debugging line
+# echo "Commit message: $COMMIT_MSG"  # Debugging line
 
-# Check the event name and whether the build should be triggered
-echo "Event Name: $GITHUB_EVENT_NAME"  # Debugging line
+# # Check the event name and whether the build should be triggered
+# echo "Event Name: $GITHUB_EVENT_NAME"  # Debugging line
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == schedule ||
       "$GITHUB_EVENT_NAME" == workflow_dispatch ||
-      "$COMMIT_MSG" =~ \[cd\ build\] ||
-      "$GITHUB_REF" == "refs/heads/main" ]]; then
+      "$COMMIT_MSG" =~ \[cd\ build\] ]]; then
+      #"$GITHUB_REF" == "refs/heads/main" ]]; then
     echo "build=true" >> $GITHUB_OUTPUT
 else
     echo "build=false" >> $GITHUB_OUTPUT  # Optional: Output 'false' for debugging
